8c43e4d2117376d15e0e11abceff2553ef51aeb2
ogetDev02 <cristian.mosquera@appogetws.com>
2021-12-31 11:57:00 -0500

    primer commit de frontend angular

---
+++ src/test/java/com/oget/ogetpro/OgetFrontend1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oget.ogetpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OgetFrontend1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
